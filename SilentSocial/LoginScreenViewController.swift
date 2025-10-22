2a473fb3d87dd173c461f78e2c0cc40b0ec849aa
Nicholas Ng <130612892+nicholas-ng-06@users.noreply.github.com>
2025-10-22 01:06:10 -0500

    Add files via upload

--- SilentSocial/LoginScreenViewController.swift
+++ SilentSocial/LoginScreenViewController.swift
@@ -10,9 +10,15 @@
 import UIKit
 // Import for login functionalities
 import FirebaseAuth
+// Import for access to usernames
+import FirebaseFirestore
 
 class LoginScreenViewController: UIViewController, UITextFieldDelegate {
     // Initial Screen that pops up for login
+    
+    // Firestore setup
+    let db = Firestore.firestore()
+    let usersCollection = "users"
     
     // Define outlets
     @IBOutlet weak var userLoginTextField: UITextField!
@@ -52,15 +58,71 @@ class LoginScreenViewController: UIViewController, UITextFieldDelegate {
     }
     
     @IBAction func loginButtonPressed(_ sender: Any) {
-        // When pressing the login button - tries to login user
-        Auth.auth().signIn(withEmail: userLoginTextField.text!, password: passwordLoginTextField.text!) { (result, error) in
-            if let error = error as NSError? {
-                self.errorLabel.text = "Error:  \(error.localizedDescription)"
+            // When pressing the login button - tries to login user
+            
+            if let loginInput = userLoginTextField.text, let password = passwordLoginTextField.text {
+                // If fields are empty return an error label message
+                if loginInput.isEmpty || password.isEmpty {
+                    self.errorLabel.text = "Please enter both login and password."
+                    return
+                }
+                // Function to handle the final Firebase Sign-In result
+                let handleSignInResult: (AuthDataResult?, Error?) -> Void = { [weak self] (result, error) in
+                    if let self = self {
+                        if let error = error as NSError? {
+                            self.errorLabel.text = "Login Error: \(error.localizedDescription)"
+                        } else {
+                            self.errorLabel.text = ""
+                            // Success handled in the view load with the listener I think
+                        }
+                    }
+                }
+                
+                // Check if username or email with checking the @
+                if loginInput.contains("@") {
+                    // Input is email, sign-in with normal auth function
+                    Auth.auth().signIn(withEmail: loginInput, password: password, completion: handleSignInResult)
+                    
+                } else {
+                    // Looking up email in firestore based on username
+                    self.db.collection(self.usersCollection)
+                      .whereField("username", isEqualTo: loginInput)
+                      .limit(to: 1) // Only need one result
+                      .getDocuments { [weak self] (querySnapshot, error) in
+                        
+                        if let self = self {
+                            if let error = error {
+                                // Check failed due to network probably
+                                self.errorLabel.text = "Network Error: Could not verify username."
+                                print("Error checking username: \(error.localizedDescription)")
+                                return
+                            }
+                            
+                            // Check for document and username
+                            if let document = querySnapshot?.documents.first {
+                                //  Extract email
+                                let data = document.data()
+                                if let email = data["email"] as? String {
+                                    // Attempt the final sign-in with the retrieved email
+                                    Auth.auth().signIn(withEmail: email, password: password, completion: handleSignInResult)
+                                } else {
+                                    // If failed here then somehow missing email field - not sure how this would happen
+                                    self.errorLabel.text = "Account data missing email field."
+                                }
+                            } else {
+                                // No document found with that username
+                                self.errorLabel.text = "User not found. Check username or email."
+                                return
+                            }
+                        }
+                    }
+                }
+                
             } else {
-                self.errorLabel.text = ""
+                self.errorLabel.text = "Please enter both login and password."
+                return
             }
         }
-    }
 
     // The following functions are to dismss the keyboard
     // Called when 'return' key pressed
---
+++ SilentSocial/ProfileSetupViewController.swift
@@ -0,0 +1,108 @@
+//  Project: SilentSocial
+//  Names: Phuc Dinh, Nicholas Ng, Preston Tu, Rui Xue
+//  Course: CS329E
+//  ProfileSetupViewController.swift
+//  SilentSocial
+//
+//  Created by Nicholas Gia-Bao Ng on 10/20/25.
+//
+
+import UIKit
+import FirebaseAuth
+import FirebaseFirestore
+
+class ProfileSetupViewController: UIViewController {
+
+    // Firebase properties
+    let db = Firestore.firestore()
+    let usersCollection = "users"
+    
+    // Define outlets
+    @IBOutlet weak var usernameSetTextField: UITextField!
+    @IBOutlet weak var displayNameSetTextField: UITextField!
+    @IBOutlet weak var errorLabel: UILabel!
+    
+    // Define variables
+    var currentUser: User?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Set placeholders for textfields
+        usernameSetTextField.placeholder = "Unique Username (e.g., JohnD_444)"
+        displayNameSetTextField.placeholder = "Display name (e.g., John Doe)"
+        
+        // Set error label to nothing
+        errorLabel.text = ""
+        
+        // Save the current user as a variable
+        currentUser = Auth.auth().currentUser
+    }
+    
+    @IBAction func continueButtonPressed(_ sender: Any) {
+        // Upon continue button being pressed
+        // Extract text field values while stripping white space
+        if let user = currentUser,
+           let username = usernameSetTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines).lowercased(), let displayName = displayNameSetTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) {
+            
+            if username.isEmpty || displayName.isEmpty {
+                // Check to make sure both fields are filled
+                errorLabel.text = "Please enter both a username and a display name"
+                // End function
+                return
+            }
+            
+            // Check for duplicate usernames
+            db.collection(usersCollection).whereField("username", isEqualTo: username).getDocuments { [weak self] (querySnapshot, error) in
+                if let self = self {
+                    if error != nil {
+                        errorLabel.text = "Network error checking username. Please try again."
+                        return
+                    }
+                    // Check if username is available - ?? true defaults to expression to true if result is null
+                    let isAvailable = querySnapshot?.documents.isEmpty ?? true
+                    
+                    if isAvailable {
+                        // Reference for specific
+                        let userRef = self.db.collection(self.usersCollection).document(user.uid)
+                        
+                        // Make a new document with the fields
+                        let userData: [String: Any] = [
+                            "username": username,
+                            "displayName": displayName,
+                            "email": user.email ?? "",
+                            "createdAt": FieldValue.serverTimestamp() // Timestamp
+                            // Add profile picture field later when existing
+                        ]
+                        
+                        // Set the data
+                        userRef.setData(userData) { [weak self] error in
+                            if let self = self {
+                                if let error = error {
+                                    errorLabel.text = "Failed to create profile: \(error.localizedDescription)"
+                                } else {
+                                    //Profile created and dismiss authentication stack
+                                    if let presentingVC = self.presentingViewController {
+                                        self.dismiss(animated: true) {
+                                            self.performSegue(withIdentifier: "profileCompleteSegue", sender: self)
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    
+                } else {
+                    // Reaching here means the username is taken
+                    self?.errorLabel.text = "The username you entered is already taken. Please choose another one."
+                    return
+                }
+            }
+        } else {
+            // If above statement fails probably weird things happened with authentication
+            errorLabel.text = "Authentication error. Please restart application."
+            return
+        }
+    }
+    
+}
